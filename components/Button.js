1de6166ee293c27083205f2e3a3a2e70d1b0c033
ejidike esther <ejidikeesther@gmail.com>
2022-01-04 17:11:23 +0100

    Add Hero

--- components/Button.js
+++ components/Button.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components'
 
+
 const ButtonComponent = styled.button`
-    background: #0071E3;
-    padding: 0.5rem 1rem;
-    border-radius: 6px;
-`;
+        background: #0071E3;
+        padding: 1rem 2rem;
+        border-radius: 6px;
+    `;
+
 
 function Button({ children }) {
+
+    
+
     return (
         <>
             <ButtonComponent>
--- components/Hero.js
+++ components/Hero.js
@@ -1,11 +1,28 @@
+import Image from "next/image";
+import Button from "./Button";
+
 function Hero() {
     return (
-        <div className="purple-gradient rounded p-5 text-white">
-            <div className="max-w-xs">
-                <h2 className="text-xl leading-8">Hi! I&apos;m Esther - <br/>
-                    a frontend developer <br/>
-                    based in Lagos, Nigeria
-                </h2>
+        <div className="purple-gradient rounded-3xl p-6 py-10 md:px-10 lg:px-16 text-white">
+            <div className="flex justify-between items-end">
+                <div className="">
+                    <h2 className="text-xl md:text-3xl lg:text-5xl leading-8 md:leading-relaxed lg:leading-relaxed md:font-semibold">Hi! I&apos;m Esther - <br/>
+                        a frontend developer <br/>
+                        based in Lagos, Nigeria
+                    </h2>
+                    <p className="text-gray-50 text-sm md:text-md lg:text-lg md:leading-6 lg:leading-8 font-extralight mt-5 max-w-xs lg:max-w-md">
+                        I have over 2 years experience in creating software
+                        solutions using JavaScript. I enjoy solving problems and
+                        making things work.
+                    </p>
+                    <div className="mt-4">
+                        <Button>Get in touch</Button>
+                        <span className="px-5 text-sm">View Work</span>
+                    </div>
+                </div>
+                <div className="border-2 rounded-full p-2 hidden sm:block ">
+                    <Image className="object-bottom object-cover " src='/images/memoji.svg' alt='Esther Ejidike - Memoji' width={250} height={250} />
+                </div>
             </div>
         </div>
     );
