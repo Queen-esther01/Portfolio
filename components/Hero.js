a35a64249f340dba1514af00bc8d106d0d6e7dcc
ejidike esther <ejidikeesther@gmail.com>
2022-01-04 15:18:33 +0100

    Escape Character In Hero

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,7 @@ function Hero() {
     return (
         <div className="purple-gradient rounded p-5 text-white">
             <div className="max-w-xs">
-                <h2 className="text-xl leading-8">Hi! I'm Esther - <br/>
+                <h2 className="text-xl leading-8">Hi! I&apos;m Esther - <br/>
                     a frontend developer <br/>
                     based in Lagos, Nigeria
                 </h2>
