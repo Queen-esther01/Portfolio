502a924bc7e78eb50b8a75746d1c1fb18a91ca37
ejidike esther <ejidikeesther@gmail.com>
2022-01-05 16:02:22 +0100

    Add InterAd

--- components/Projects.js
+++ components/Projects.js
@@ -6,9 +6,9 @@ function Projects() {
     return (
         <div className="bg-gray text-white max-w-none w-full m-auto py-10">
             <div className="mobile-width max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl m-auto">
-                <h2 className="lg:max-w-4xl lg:m-auto">SOME PAST PROJECTS</h2>
-                <div className="my-10 md:py-10 lg:m-auto lg:max-w-4xl flex flex-col items-center md:flex-row md:justify-between lg:justify-between">
-                    <div className="my-5 md:w-2/4">
+                <h2 className="lg:max-w-4xl lg:m-auto text-sm">SOME PAST PROJECTS</h2>
+                <div className="my-10 md:py-10 lg:m-auto lg:max-w-4xl flex flex-col items-center sm:flex-row md:justify-between lg:justify-between">
+                    <div className="my-5 sm:w-2/4 ">
                         <h2 className="text-3xl font-semibold lg:text-5xl">Levare</h2>
                         <p className="my-5">Levare is a new upcoming financial technology company that provides financial solutions to individuals. I developed the website and set up domain, Dns configurations and SSl.</p>
                         <p className="text-slate-400 my-5 mb-6">Technology Used: React, Nextjs,<br/> Animate.css, Sass, Netlify.</p>
@@ -18,8 +18,23 @@ function Projects() {
                             </Link>
                         </Button>
                     </div>
-                    <div className="m-auto md:text-right md:m-0 md:w-2/5 md:h-2/5 mt-10">
-                        <Image className="object-contain" src='/images/levare.svg' alt='Levare'  width={350} height={350} />
+                    <div className="m-auto sm:text-right md:m-0 sm:w-2/4 sm:h-2/4 mt-10 ">
+                        <Image className="object-contain" src='/images/levare.svg' alt='Levare'  width={400} height={400} />
+                    </div>
+                </div>
+                <div className="my-20 md:py-16 lg:m-auto lg:max-w-4xl flex flex-col items-center sm:flex-row-reverse md:justify-between lg:justify-between">
+                    <div className="my-5 sm:w-2/4 ">
+                        <h2 className="text-3xl font-semibold lg:text-5xl">InterAd</h2>
+                        <p className="my-5">InterAd is an online engagement and marketing portal which businesses can use to create incentivised digital promotions with the aim of attracting more customers beyond the usual circle and also gather valuable information for business decisions.</p>
+                        <p className="text-slate-400 my-5 mb-6">Technology Used: HTML, CSS, JavaScript, Jquery, <br/> Animate.css, GSAP, Particles.js, Swiper.js etc</p>
+                        <Button>
+                            <Link href='https://interad.online' >
+                                Visit Website
+                            </Link>
+                        </Button>
+                    </div>
+                    <div className="m-auto sm:text-left md:m-0 sm:w-2/4 sm:h-2/4 mt-10">
+                        <Image className="object-contain" src='/images/InterAd.svg' alt='Levare'  width={400} height={400} />
                     </div>
                 </div>
             </div>
