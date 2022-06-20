4cb1a9b05b0d7a81813f5e856445c1c45f6e8226
Esther <ejidikeesther@gmail.com>
2022-06-20 02:25:08 +0100

    Add New Portfolio

--- components/Projects.js
+++ components/Projects.js
@@ -126,6 +126,27 @@ function Projects() {
                 {/* <h2 className="lg:max-w-4xl lg:m-auto text-sm">PERSONAL WORK</h2> */}
                 <div className={`${category === 'Personal' ? 'block' : 'hidden'} md:py-10 lg:m-auto lg:max-w-4xl flex flex-col items-center sm:flex-row-reverse md:justify-between lg:justify-between`}>
                     <div className="my-5 sm:w-2/4 ">
+                        <h2 className="text-3xl font-semibold lg:text-5xl">E-Commerce App</h2>
+                        <p className="my-5">My store is an e-commerece web application leveraging the fakestore API on a graphql-server on the server side and apollo-client on the client side. It features displaying products by categories, displaying single products, adding and deleting to cart using Reactive variables.</p>
+                        <p className="text-slate-400 my-5 mb-6">Technologies Used: React, Typescript, graphql, <br/> apollo-client, Tailwind etc</p>
+                        <Button>
+                            <Link href='https://mystore-graphql-client.vercel.app/' >
+                                View App
+                            </Link>
+                        </Button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Button>
+                            <Link href='https://github.com/Queen-esther01/mystore-graphql-server' >
+                                View Server
+                            </Link>
+                        </Button>
+                    </div>
+                    <div className="m-auto sm:text-left md:m-0 sm:w-2/4 sm:h-2/4 mt-10">
+                        <Image priority className="object-contain" src='/images/mystore.png' alt='mystore - Developer(Esther)'  width={400} height={400} />
+                    </div>
+                </div>
+                <div className={`${category === 'Personal' ? 'block' : 'hidden'} md:py-10 lg:m-auto lg:max-w-4xl flex flex-col items-center sm:flex-row md:justify-between lg:justify-between`}>
+                    <div className="my-5 sm:w-2/4 ">
                         <h2 className="text-3xl font-semibold lg:text-5xl">React Npm Package</h2>
                         <p className="my-5">This package checks the strength of a password by checking against these factors: 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character. It also exposes a function that returns results based on password inputs.</p>
                         <p className="text-slate-400 my-5 mb-6">Technologies Used: React, Jest,<br/> CSS, Babel etc</p>
@@ -145,7 +166,7 @@ function Projects() {
                         <Image priority className="object-contain" src='/images/strengthbar.png' alt='Primecourts - Developer(Esther)'  width={400} height={400} />
                     </div>
                 </div>
-                <div className={`${category === 'Personal' ? 'block' : 'hidden'} my-20 md:py-10 lg:m-auto lg:max-w-4xl flex flex-col items-center sm:flex-row md:justify-between lg:justify-between`}>
+                <div className={`${category === 'Personal' ? 'block' : 'hidden'} my-20 md:py-10 lg:m-auto lg:max-w-4xl flex flex-col items-center sm:flex-row-reverse md:justify-between lg:justify-between`}>
                     <div className="my-5 sm:w-2/4 ">
                         <h2 className="text-3xl font-semibold lg:text-5xl">The movies</h2>
                         <p className="my-5">The movies is a movie app that provides detailed information on movies (cast and plot). It also allows users search all movies by genre and search option.</p>
@@ -160,7 +181,7 @@ function Projects() {
                         <Image priority className="object-contain" src='/images/themovies.png' alt='Primecourts - Developer(Esther)'  width={400} height={400} />
                     </div>
                 </div>
-                <div className={`${category === 'Personal' ? 'block' : 'hidden'} my-20 md:py-16 lg:m-auto lg:max-w-4xl flex flex-col sm:flex-row-reverse md:justify-between lg:justify-between`}>
+                <div className={`${category === 'Personal' ? 'block' : 'hidden'} my-20 md:py-16 lg:m-auto lg:max-w-4xl flex flex-col sm:flex-row md:justify-between lg:justify-between`}>
                     <div className="my-5 sm:w-2/4 ">
                         <h2 className="text-3xl font-semibold lg:text-5xl">Drum Machine</h2>
                         <p className="my-5">The drum machine app emits different drum sounds based on clicked keypads. It includes toggles to either switch drum on and off or switch from drum to piano sounds. The keypads can also be triggered by their respective keys on the keyboard. e.g open app and click letter &apos;w&apos; on keyboard.</p>
@@ -175,7 +196,7 @@ function Projects() {
                         <Image priority className="object-contain" src='/images/drummachine.png' alt='Primecourts - Developer(Esther)'  width={400} height={400} />
                     </div>
                 </div>
-                <div className={`${category === 'Personal' ? 'block' : 'hidden'} my-20 md:py-16 lg:m-auto lg:max-w-4xl flex flex-col sm:flex-row md:justify-between lg:justify-between`}>
+                <div className={`${category === 'Personal' ? 'block' : 'hidden'} my-20 md:py-16 lg:m-auto lg:max-w-4xl flex flex-col sm:flex-row-reverse md:justify-between lg:justify-between`}>
                     <div className="my-5 sm:w-2/4 ">
                         <h2 className="text-3xl font-semibold lg:text-5xl">Random Quotes</h2>
                         <p className="my-5">A simple app that generates random quotes from random authors.</p>
@@ -190,7 +211,7 @@ function Projects() {
                         <Image priority className="object-contain" src='/images/randomquotes.png' alt='Primecourts - Developer(Esther)'  width={400} height={400} />
                     </div>
                 </div>
-                <div className={`${category === 'Personal' ? 'block' : 'hidden'} my-20 md:py-16 lg:m-auto lg:max-w-4xl flex flex-col sm:flex-row-reverse md:justify-between lg:justify-between`}>
+                <div className={`${category === 'Personal' ? 'block' : 'hidden'} my-20 md:py-16 lg:m-auto lg:max-w-4xl flex flex-col sm:flex-row md:justify-between lg:justify-between`}>
                     <div className="my-5 sm:w-2/4 ">
                         <h2 className="text-3xl font-semibold lg:text-5xl">Markdown Previewer</h2>
                         <p className="my-5">A simple app that parses markdown text in realtime.</p>
