447b119255fda6625fb94bc8c5443c4ec3a8bc43
Esther <ejidikeesther@gmail.com>
2022-08-11 23:38:19 +0100

    Edit Hero

--- components/Hero.js
+++ components/Hero.js
@@ -8,11 +8,11 @@ function Hero() {
             <div className="purple-gradient rounded-3xl p-6 py-10 md:px-10 lg:px-16 text-white mobile-width max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl m-auto">
                 <div className="flex justify-between items-end">
                     <div className="">
-                        <h2 className="text-xl md:text-3xl lg:text-5xl leading-8 md:leading-relaxed lg:leading-relaxed md:font-semibold">
-                            Hi! I&apos;m Esther -
-                            a Frontend Engineer 
-                            based in Lagos, Nigeria
-                        </h2>
+                        <div className="text-xl md: lg:text-4xl leading-8 md:leading-relaxed lg:leading-relaxed md:font-semibold">
+                            Hi! I&apos;m Esther,
+                            a frontend engineer<br/>
+                            - living in Lagos, Nigeria
+                        </div>
                         <p className="text-gray-50 text-sm md:text-md lg:text-lg md:leading-6 lg:leading-8 font-extralight mt-5 max-w-xs lg:max-w-md">
                         With over 2 years of experience, I am adept at
 capturing the intersection between development and design through rich
@@ -25,13 +25,16 @@ team I become a part of😊.
                                     Get in touch
                                 </Link>
                             </button>
-                            <span className="px-8 text-sm">
+                            <span className="px-8 sm:px-4 md:px-8 text-sm">
                                 <a href='/Esther-Ejidike-CV.pdf' download>Download CV</a>
                             </span>
                         </div>
                     </div>
-                    <div className="rounded-full p-2 hidden sm:block ">
-                        <Image className="object-bottom object-cover" src='/images/rocket.png' alt='Esther Ejidike - Memoji' width={250} height={250} />
+                    <div className="rounded-full p-2 hidden sm:block lg:hidden">
+                        <Image className="object-bottom object-cover" src='/images/rocket.png' alt='Esther Ejidike - Memoji' width={250} height={300} />
+                    </div>
+                    <div className="rounded-full p-2 hidden lg:block">
+                        <Image className="object-bottom object-cover" src='/images/rocket.png' alt='Esther Ejidike - Memoji' width={400} height={400} />
                     </div>
                 </div>
             </div>
